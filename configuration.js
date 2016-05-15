ca5bf5e237d7946ca04af51dbdf5886cdfbe4ac2
anand <anand.pathak@innoraft.com>
2016-05-15 21:03:29 +0530

    initial commit gmail bot

---
+++ configuration.js
@@ -0,0 +1,24 @@
+var CONFIG= {
+	search : {
+		/*gmail search parameter 
+			e.g  'from : himesh@reshamiya.com'
+		*/
+		mail_search : ""
+		/*Maximum number of emails*/
+		,maxResults : 1000
+		/* filter using label*/
+		,labelIds : 'unread'
+		,includeSpamTrash : false
+	}
+	,
+	Rules :[
+		{
+			type : 'keyword' /*keyword, regex , send_from , */
+			,action : ''		/*reply , delete , add_lebel , mark_as_read,count*/
+			,value : ['']		/*message*/
+		}
+
+	]
+
+}
+module.exports = CONFIG;
